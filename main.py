46cfa1178e4db2082c7a44fca38f7a941c729767
Vadim Devlekamov <devlevadim84@mail.ru>
2024-12-21 01:31:00 +0600

    Change import and names functions in main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_date, mask_account_card
 
 """Импортируем функции из пакета src"""
-print(get_mask_account("73654108430135874305"))
-print(get_mask_card_number("7000792289606361"))
+print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Maestro 7000792289606361"))
+print(mask_account_card("Счет 73654108430135874305"))
+print(get_date("2024-03-11T02:26:18.671407"))
