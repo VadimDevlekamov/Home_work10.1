2444484a68dc894dcef883019442a8940a125588
Vadim Devlekamov <devlevadim84@mail.ru>
2024-12-26 20:55:48 +0600

    Greate functions sort_by_date for sorted by key date

--- src/processing.py
+++ src/processing.py
@@ -8,4 +8,9 @@ def filter_by_state(data: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
 
     return [item for item in data if item.get('state') == state]
 
+def sort_by_date(data: List[Dict], decrease: bool = True) -> List[Dict]:
 
+    """    Функция сортирует список словарей по ключу 'date'.    """
+
+
+    return sorted(data, key=lambda x: x['date'], reverse=decrease)
