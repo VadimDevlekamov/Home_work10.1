0e6cd36383a036d7c71a8d8fadd1143087f764dd
Vadim Devlekamov <devlevadim84@mail.ru>
2025-01-14 21:07:47 +0600

    greate transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -1,7 +1,14 @@
 from typing import List, Dict, Any, Iterator
 
+
 def filter_by_currency(transactions: List[Dict[str, Any]], currency: str) -> Iterator[Dict[str, Any]]:
     """Генератор, который фильтрует транзакции по валюте."""
     for transaction in transactions:
         if transaction["operationAmount"]["currency"]["code"] == currency:
             yield transaction
+
+
+def transaction_descriptions(transactions: List[Dict[str, Any]]) -> Iterator[str]:
+    """Генератор, возвращающий описания транзакций."""
+    for transaction in transactions:
+        yield transaction["description"]
