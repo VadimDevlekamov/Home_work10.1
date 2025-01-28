ea2bd4c183c39a26d6e4b0700335f041066ae1d9
Vadim Devlekamov <devlevadim84@mail.ru>
2025-01-28 21:03:44 +0600

    fixing errors

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,7 @@
 import re
 from datetime import datetime
 
-from .masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_info: str) -> str:
